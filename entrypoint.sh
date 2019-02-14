5963ccb630d6f5b40524f456da2fd507f858b554
Jasoet <deny.prasetyo@go-pay.co.id>
2019-02-15 01:11:43 +0530

    [Jasoet] Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+echo "#################################"
+echo "Enable sdkman executable"
+echo "#################################"
 export SDKMAN_DIR="/root/.sdkman"
- [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+[[ -s "/root/.sdkman/bin/sdkman-init.sh" ]] && source "/root/.sdkman/bin/sdkman-init.sh"
 
-$@
+
+echo "#################################"
+echo "Execute Command!"
+echo "#################################"
+if [ -x /usr/local/bin/bash ]; then
+    exec /usr/local/bin/bash $@
+elif [ -x /usr/bin/bash ]; then
+    exec /usr/bin/bash $@
+elif [ -x /bin/bash ]; then
+    exec /bin/bash $@
+elif [ -x /usr/local/bin/sh ]; then
+    exec /usr/local/bin/sh $@
+elif [ -x /usr/bin/sh ]; then
+    exec /usr/bin/sh $@
+elif [ -x /bin/sh ]; then
+    exec /bin/sh $@
+else
+    echo shell not found
+    exit 1
+fi
+
