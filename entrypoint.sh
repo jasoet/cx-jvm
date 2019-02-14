4bf563d5810f09384acff3757a16ce7a34784f20
Jasoet <deny.prasetyo@go-pay.co.id>
2019-02-15 02:04:47 +0530

    [Jasoet] Remove comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ echo "##### Enable sdkman executable #####"
 export SDKMAN_DIR="/root/.sdkman"
 [[ -s "/root/.sdkman/bin/sdkman-init.sh" ]] && source "/root/.sdkman/bin/sdkman-init.sh"
 
-echo "##### Execute Command! ${@} #####"
+echo "##### Execute Command! #####"
 exec "$@"
 
