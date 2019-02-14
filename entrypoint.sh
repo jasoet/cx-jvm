3bca447dad4784f33749fd7925089d52807b48ca
Jasoet <deny.prasetyo@go-pay.co.id>
2019-02-15 01:51:22 +0530

    [Jasoet] Update endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,5 @@ export SDKMAN_DIR="/root/.sdkman"
 [[ -s "/root/.sdkman/bin/sdkman-init.sh" ]] && source "/root/.sdkman/bin/sdkman-init.sh"
 
 echo "##### Execute Command! ${@} #####"
-if [[ -x /usr/local/bin/bash ]]; then
-    exec /usr/local/bin/bash $@
-elif [[ -x /usr/bin/bash ]]; then
-    exec /usr/bin/bash $@
-elif [[ -x /bin/bash ]]; then
-    exec /bin/bash $@
-elif [[ -x /usr/local/bin/sh ]]; then
-    exec /usr/local/bin/sh $@
-elif [[ -x /usr/bin/sh ]]; then
-    exec /usr/bin/sh $@
-elif [[ -x /bin/sh ]]; then
-    exec /bin/sh $@
-else
-    echo shell not found
-    exit 1
-fi
+exec "$@"
 
