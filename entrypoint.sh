de1db778234a6852d14966686875f55ffdfc5be9
Jasoet <deny.prasetyo@go-pay.co.id>
2019-02-15 01:22:49 +0530

    [Jasoet] specify image version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
-echo "#################################"
-echo "Enable sdkman executable"
-echo "#################################"
+echo "##### Enable sdkman executable #####"
 export SDKMAN_DIR="/root/.sdkman"
 [[ -s "/root/.sdkman/bin/sdkman-init.sh" ]] && source "/root/.sdkman/bin/sdkman-init.sh"
 
-
-echo "#################################"
-echo "Execute Command!"
-echo "#################################"
-if [ -x /usr/local/bin/bash ]; then
-    exec /usr/local/bin/bash $@
-elif [ -x /usr/bin/bash ]; then
-    exec /usr/bin/bash $@
-elif [ -x /bin/bash ]; then
-    exec /bin/bash $@
-elif [ -x /usr/local/bin/sh ]; then
-    exec /usr/local/bin/sh $@
-elif [ -x /usr/bin/sh ]; then
-    exec /usr/bin/sh $@
-elif [ -x /bin/sh ]; then
-    exec /bin/sh $@
-else
-    echo shell not found
-    exit 1
-fi
+echo "##### Execute Command! #####"
+exec $@
 
